843e8187b86600d311d9ab323b395b7a086a5d2c
Саша Чернов <shooter-sniper@mail.ru>
2020-04-06 11:29:46 +0400

    Нужны изменения

---
+++ DistanceLesson1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DistanceLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new Model.LinkedList<int>();
+            list.add(1);
+            list.add(2);
+            list.add(3);
+
+            Console.ReadLine();
+        }
+    }
+}
